ad3c1bed2679c74b6e009fb618b9af8ff42a216f
Shiv 70 <shivamghadi07@gmail.com>
2026-08-24 19:45:00 +0530

    Ok

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql2/promise");
+
 require("dotenv").config();
 
 const db = mysql.createPool({
@@ -6,10 +7,15 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),
+
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = db;
