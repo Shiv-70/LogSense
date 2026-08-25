c1f569ac939b28ecec1f0b2f69eea0aaf5abe0ff
Shiv 70 <shivamghadi07@gmail.com>
2026-08-25 21:59:07 +0530

    API connection solve

--- server/src/server.js
+++ server/src/server.js
@@ -31,7 +31,7 @@ app.get("/api/health", async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 18891;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
